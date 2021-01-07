09dbf4ed050ea1f73edcff9ed568a796e1993cb7
Alex Gusev <alex@flancer64.com>
2021-01-07 11:19:41 +0200

    LEAN-115 Add sign out route to desk realm

--- src/Defaults.mjs
+++ src/Defaults.mjs
@@ -2,39 +2,14 @@
  * Hardcoded configuration for the module.
  */
 export default class Fl32_Teq_User_Defaults {
-    /**
-     * Number of salt rounds used in bcrypt hash.
-     * @type {Number}
-     * @default
-     */
-    BCRYPT_HASH_ROUNDS = 10;
-    /**
-     * ID of the session singleton in DI-container.
-     */
-    DI_SESSION = 'session';
-    /**
-     * Attribute of the HTTP request to store session ID.
-     * @type {string}
-     */
-    HTTP_REQ_SESSION_ID = 'sessionId';
-    /**
-     * Attribute of the HTTP request to store authenticated user data.
-     * @type {string}
-     */
-    HTTP_REQ_USER = 'user';
-    /**
-     * @type {string}
-     */
-    SESSION_COOKIE_NAME = 'TEQ_SESSION_ID';
-    /**
-     * @type {number}
-     */
+    BCRYPT_HASH_ROUNDS = 10;    // Number of salt rounds used in bcrypt hash.
+    DI_SESSION = 'session'; // ID of the session singleton in DI-container.
+    HTTP_REQ_SESSION_ID = 'sessionId';  // Attribute of the HTTP request to store session ID.
+    HTTP_REQ_USER = 'user'; // Attribute of the HTTP request to store authenticated user data.
+    ROUTE_SIGN_IN = '/user/signIn';
     SESSION_COOKIE_LIFETIME = 31536000000;  // 3600 * 24 * 365 * 1000
-    /**
-     * Number of bytes for generated session ID.
-     * @type {number}
-     */
-    SESSION_ID_BYTES = 20;
+    SESSION_COOKIE_NAME = 'TEQ_SESSION_ID';
+    SESSION_ID_BYTES = 20;  // Number of bytes for generated session ID.
 
     constructor() {
         Object.freeze(this);
