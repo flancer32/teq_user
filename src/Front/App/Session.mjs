e5cdb4e4c2c024960443f4d9aaeba966e461ac8b
Alex Gusev <alex@flancer64.com>
2021-02-22 15:57:53 +0200

    MF-3 Sign In Route

--- src/Front/App/Session.mjs
+++ src/Front/App/Session.mjs
@@ -4,14 +4,15 @@
 export default class Fl32_Teq_User_Front_App_Session {
     constructor(spec) {
         /** @type {Fl32_Teq_User_Front_Gate_Current.gate} */
-        const gateCurrent = spec['Fl32_Teq_User_Front_Gate_Current$'];
+        const gateCurrent = spec['Fl32_Teq_User_Front_Gate_Current$']; // function singleton
         /** @type {Fl32_Teq_User_Front_Gate_Sign_Out.gate} */
         const gateSignOut = spec['Fl32_Teq_User_Front_Gate_Sign_Out$']; // function singleton
         /** @type {typeof Fl32_Teq_User_Shared_Service_Route_Current_Request} */
-        const CurrentRequest = spec['Fl32_Teq_User_Shared_Service_Route_Current#Request'];
-        const SignOutRequest = spec['Fl32_Teq_User_Shared_Service_Route_Sign_Out#Request'];
+        const CurrentRequest = spec['Fl32_Teq_User_Shared_Service_Route_Current#Request']; // class constructor
+        /** @type {typeof Fl32_Teq_User_Shared_Service_Route_Sign_Out_Request} */
+        const SignOutRequest = spec['Fl32_Teq_User_Shared_Service_Route_Sign_Out#Request']; // class constructor
         /** @type {typeof Fl32_Teq_User_Shared_Service_Data_User} */
-        const User = spec['Fl32_Teq_User_Shared_Service_Data_User#'];
+        const User = spec['Fl32_Teq_User_Shared_Service_Data_User#']; // class constructor
 
         /** @type {Fl32_Teq_User_Shared_Service_Data_User} */
         let user = null;
@@ -51,6 +52,20 @@ export default class Fl32_Teq_User_Front_App_Session {
             if (res.user) {
                 user = Object.assign(new User(), res.user);
             }
+        };
+
+        /**
+         * Redirect to sign in route in user is not authenticated. Store current route before redirect.
+         * @returns {Boolean} 'true' if user is authenticated.
+         */
+        this.checkUserAuthenticated = async function (router) {
+            const result = (user instanceof User);
+            if (!result) {
+                const routeCurrent = router.currentRoute.value.path;
+                this.setRouteToRedirect(routeCurrent);
+                router.push(routeToSignIn);
+            }
+            return result;
         };
 
         this.setRouteToRedirect = function (route) {
