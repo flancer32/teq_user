29f730b345f4caafb8c2669153054d41eaed9317
Alex Gusev <alex@flancer64.com>
2021-06-22 16:16:14 +0300

    OWN-200 Medium: TeqFW services

--- src/Plugin/Init.mjs
+++ src/Plugin/Init.mjs
@@ -8,31 +8,10 @@ export default class Fl32_Teq_User_Plugin_Init {
         /** @type {Fl32_Teq_User_Defaults} */
         const DEF = spec['Fl32_Teq_User_Defaults$'];    // singleton
 
-        this.getCommands = function () {
-            return [];
-        };
-
-        this.getHttpStaticMaps = function () {
-            return {};
-        };
-
         this.getServicesRealm = function () {
             return DEF.BACK_REALM;
         };
 
-        this.getServicesList = function () {
-            return [
-                'Fl32_Teq_User_Back_Service_ChangePassword$',
-                'Fl32_Teq_User_Back_Service_Check_Existence$',
-                'Fl32_Teq_User_Back_Service_Current$',
-                'Fl32_Teq_User_Back_Service_List$',
-                'Fl32_Teq_User_Back_Service_RefLink_Create$',
-                'Fl32_Teq_User_Back_Service_RefLink_Get$',
-                'Fl32_Teq_User_Back_Service_Sign_In$',
-                'Fl32_Teq_User_Back_Service_Sign_Out$',
-                'Fl32_Teq_User_Back_Service_Sign_Up$',
-            ];
-        };
     }
 
 }
