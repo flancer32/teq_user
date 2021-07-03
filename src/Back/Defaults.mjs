8d675d37428fcab634d4d80d449dd820ea98e8f9
Alex Gusev <alex@flancer64.com>
2021-07-03 14:03:20 +0300

    WF-457 Create @teqfw/web plugin

--- src/Back/Defaults.mjs
+++ src/Back/Defaults.mjs
@@ -4,7 +4,6 @@
 export default class Fl32_Teq_User_Back_Defaults {
     BACK_REALM = 'user';
     BCRYPT_HASH_ROUNDS = 10;    // Number of salt rounds used in bcrypt hash.
-    DI_SESSION = 'userSession'; // ID of the session singleton in DI-container.
 
     HTTP_SHARE_CTX_SESSION_ID = null; // bind in constructor
     HTTP_SHARE_CTX_USER = null; // bind in constructor
@@ -18,15 +17,6 @@ export default class Fl32_Teq_User_Back_Defaults {
         /** @type {TeqFw_Web_Back_Defaults} */
         WEB: null, // bind in constructor
     };
-
-    SERV_CHANGE_PASSWORD = '/changePassword';
-    SERV_CHECK_EXISTENCE = '/check/existence';
-    SERV_LIST = '/list';
-    SERV_REF_LINK_CREATE = '/refLink/create';
-    SERV_REF_LINK_GET = '/refLink/get';
-    SERV_SIGN_IN = '/sign/in';
-    SERV_SIGN_OUT = '/sign/out';
-    SERV_SIGN_UP = '/sign/up';
 
     SESSION_COOKIE_LIFETIME = 31536000000;  // 3600 * 24 * 365 * 1000
     SESSION_COOKIE_NAME = 'TEQ_SESSION_ID';
